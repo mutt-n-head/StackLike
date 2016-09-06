96a6fd484973ece69dd80de3f066e8629096db2e
nematode195 <mike.tallsmall@gmail.com>
2016-09-06 15:47:48 -0400

    moved stackTest

--- test/stackTest.js
+++ test/stackTest.js
@@ -1,12 +1,12 @@
 var expect = require("chai").expect;
-//var stack = require("./stack");
+var stack = require("../stack");
 
 describe("pushpop", function() {
   describe("pop gets what you pushed", function() {
     it("tests stack", function() {
 	  var mystack = stack.create();
 	  mystack.push(5);
-      var mypop = stack.pop();
+      var mypop = mystack.pop();
       
       expect(mypop).to.equal(5);
       
